43f9958e577f2c3e847b486e8433b4ed34f7a21a
Loreny Ielpo <lorenyielpo@gmail.com>
2019-08-28 11:41:43 -0300

    Criação do repositório da API com mongoose

---
+++ src/js/repository/VUTTRRepository.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenvSafe = require("dotenv-safe");
+dotenvSafe.load();
+const mongoose = require("mongoose");
+const MONGO_URL = process.env.MONGODB_URI;
+function connect() {
+    mongoose.connect(MONGO_URL, { useNewUrlParser: true }, error => {
+        if (error) {
+            console.log('Falha ao conectar no mongo', error);
+        }
+        else {
+            console.log('Conectado no MongoDB');
+        }
+    });
+}
+exports.default = connect;
+//# sourceMappingURL=VUTTRRepository.js.map
--- src/ts/repository/VUTTRRepository.ts
+++ src/ts/repository/VUTTRRepository.ts
@@ -1,1 +1,19 @@
-import dotenvSafe = require('dotenv-save');
+import dotenvSafe = require('dotenv-safe');
+dotenvSafe.load();
+
+import mongoose = require('mongoose');
+const MONGO_URL = process.env.MONGODB_URI;
+
+function connect (){
+    mongoose.connect(<string> MONGO_URL,
+        { useNewUrlParser: true},
+        error => {
+            if(error) {
+                console.log('Falha ao conectar no mongo', error)
+            } else {
+                console.log('Conectado no MongoDB')
+            }
+        });
+}
+
+export default connect;
