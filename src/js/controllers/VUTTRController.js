74bab5ff376b72e832812e805ba6802d4ae96af0
Loreny Ielpo <lorenyielpo@gmail.com>
2019-08-28 20:48:25 -0300

    adicionar uma nova ferramenta

--- src/js/controllers/VUTTRController.js
+++ src/js/controllers/VUTTRController.js
@@ -10,16 +10,18 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const VUTTRRepository_1 = require("../repository/VUTTRRepository");
 const UserSchema_1 = require("../schemas/UserSchema");
+const ToolsSchema_1 = require("../schemas/ToolsSchema");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 VUTTRRepository_1.default();
+let idLogado;
 class VUTTRController {
     static getAll() {
         return UserSchema_1.default.find((error, users) => {
             return users;
         });
     }
-    getByTag(tag) {
+    static getByTag(tag) {
         return UserSchema_1.default.findOne({
             tools: {
                 tags: tag
@@ -41,27 +43,39 @@ class VUTTRController {
             return newUser.save();
         });
     }
-    login(dataLogin) {
+    static login(dataLogin) {
         return __awaiter(this, void 0, void 0, function* () {
             const user = yield UserSchema_1.default.findOne({
                 email: dataLogin.email
             });
-            const correctPassword = bcrypt.compareSync(dataLogin.password, user.password);
-            if (user && correctPassword) {
-                const token = jwt.sign({
-                    email: user.email,
-                    id: user._id
-                }, process.env.PRIVATE_KEY);
-                return { auth: true, token };
+            if (user) {
+                const correctPassword = bcrypt.compareSync(dataLogin.password, user.password);
+                if (correctPassword) {
+                    const token = jwt.sign({
+                        email: user.email,
+                        id: user._id
+                    }, process.env.PRIVATE_KEY);
+                    idLogado = user._id;
+                    return { auth: true, token };
+                }
+                else {
+                    throw new Error('Incorrect data');
+                }
             }
             else {
                 throw new Error('Incorrect data');
             }
         });
     }
-    addTools() {
+    static addTools(tool) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const user = yield UserSchema_1.default.findById(idLogado);
+            const newTool = new ToolsSchema_1.toolsModel(tool);
+            user.tools.push(newTool);
+            return user.save();
+        });
     }
-    deleteTools() {
+    static deleteTools() {
     }
 }
 exports.default = VUTTRController;
