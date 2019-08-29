c9c28154afedf1f436f52d998b3983b1a89bc6b2
Loreny Ielpo <lorenyielpo@gmail.com>
2019-08-29 17:03:02 -0300

    correção server

--- src/js/server.js
+++ src/js/server.js
@@ -2,167 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv = require("dotenv-safe");
 dotenv.config();
-const express = require("express");
 const bodyParser = require("body-parser");
+const express = require("express");
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
-const Login_1 = require("./helpers/Login");
-const UserController_1 = require("./controllers/UserController");
-const ToolsController_1 = require("./controllers/ToolsController");
+const Routes_1 = require("./Routes");
 const PORT = parseInt(process.env.PORT, 10) || 7000;
 const server = express();
 server.use(bodyParser());
 server.use(cors());
 server.use(bodyParser.json());
-server.get('/', (request, response) => {
-    response.send('Hello').status(200);
-});
-server.get('/v1/user', (request, response) => {
-    UserController_1.default.getAllUser()
-        .then(users => response.send(users).status(200))
-        .catch(error => {
-        if (error.name == 'CastError') {
-            response.sendStatus(400);
-        }
-        else {
-            response.sendStatus(500);
-        }
-    });
-});
-server.post('/v1/user', (request, response) => {
-    UserController_1.default.addUser(request.body)
-        .then(user => {
-        const _id = user._id;
-        response.send(_id).status(201);
-    })
-        .catch(error => {
-        if (error.name === 'ValidationError') {
-            response.sendStatus(400);
-        }
-        else {
-            response.sendStatus(500);
-        }
-    });
-});
-server.delete('/v1/user/:idUser', (request, response) => {
-    const idUser = request.params.idUser;
-    UserController_1.default.deleteUser(idUser)
-        .then(() => {
-        response.sendStatus(204);
-    })
-        .catch(error => {
-        if (error.name == 'CastError') {
-            response.sendStatus(400);
-        }
-        else {
-            response.sendStatus(500);
-        }
-    });
-});
-server.post('/v1/login', (request, response) => {
-    Login_1.default.login(request.body)
-        .then(login => {
-        response.send(login).status(200);
-    })
-        .catch(error => {
-        if (error.name === 'ValidationError') {
-            response.sendStatus(400);
-        }
-        else {
-            response.sendStatus(500);
-        }
-    });
-});
-server.get('/v1/tools', (request, response) => {
-    const tag = request.query.tag;
-    if (!tag) {
-        ToolsController_1.default.getTools()
-            .then(tools => response.send(tools).status(200))
-            .catch(error => {
-            if (error.name == 'CastError') {
-                response.sendStatus(400);
-            }
-            else {
-                response.sendStatus(500);
-            }
-        });
-    }
-    else {
-        ToolsController_1.default.getByTag(tag)
-            .then(tools => response.send(tools).status(200))
-            .catch(error => {
-            if (error.name == 'CastError') {
-                response.sendStatus(400);
-            }
-            else {
-                response.sendStatus(500);
-            }
-        });
-    }
-});
-server.post('/v1/tools', (request, response) => {
-    const authHeader = request.get('authorization');
-    let auth = false;
-    if (authHeader) {
-        const token = authHeader.split(' ')[1];
-        jwt.verify(token, process.env.PRIVATE_KEY, (error, decoded) => {
-            if (error) {
-                response.send(403);
-            }
-            else {
-                auth = true;
-            }
-        });
-    }
-    else {
-        response.send(401);
-    }
-    if (auth) {
-        ToolsController_1.default.addTools(request.body)
-            .then(tools => response.send(tools).status(201))
-            .catch(error => {
-            if (error.name === 'ValidationError') {
-                response.sendStatus(400);
-            }
-            else {
-                response.sendStatus(500);
-            }
-        });
-    }
-});
-server.delete('/v1/tools/:idTool', (request, response) => {
-    const authHeader = request.get('authorization');
-    let auth = false;
-    if (authHeader) {
-        const token = authHeader.split(' ')[1];
-        jwt.verify(token, process.env.PRIVATE_KEY, (error, decoded) => {
-            if (error) {
-                response.send(403);
-            }
-            else {
-                auth = true;
-            }
-        });
-    }
-    else {
-        response.send(401);
-    }
-    if (auth) {
-        const idTool = request.params.idTool;
-        ToolsController_1.default.deleteTools(idTool)
-            .then(() => {
-            response.sendStatus(204);
-        })
-            .catch(error => {
-            if (error.name == 'CastError') {
-                response.sendStatus(400);
-            }
-            else {
-                response.sendStatus(500);
-            }
-        });
-    }
-});
+const rotas = new Routes_1.default(server);
+rotas.router();
 server.listen(PORT);
 console.info(`Rodando na porta ${PORT}`);
-//# sourceMappingURL=server.js.map
+//# sourceMappingURL=Server.js.map
