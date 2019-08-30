8a849120e241fb27e2fe835d1a8e1a1908bc6ee1
Loreny Ielpo <lorenyielpo@gmail.com>
2019-08-29 23:34:34 -0300

    Inclusão da propriedade link no toolschema

--- src/ts/schemas/ToolsSchema.ts
+++ src/ts/schemas/ToolsSchema.ts
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const ToolsSchema = new Schema({
     _id: {type: mongoose.Schema.Types.ObjectId, auto: true},
     title: {type: String, required: true},
+    link: {type: String, required: true},
     description: {type: String, required: true},
     tags: {type: Array, required: true},
     author: {type: String}
